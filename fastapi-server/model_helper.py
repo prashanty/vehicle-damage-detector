dcdb4e160aafcce9add3b4d201d016173a85414f
Prashant Yadav <prashant@Prashants-MacBook-Air.local>
2026-07-17 11:49:59 +0530

    Added the fastapi backend server

---
+++ fastapi-server/model_helper.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import torch
+from torch import nn
+from torchvision import transforms, models
+
+trained_model = None
+class_names = ['Front_Breakage', 'Front_Crushed', 'Front_Normal', 'Rear_Breakage', 'Rear_Crushed', 'Rear_Normal']
+
+# Load the pre-trained ResNet model
+class CarClassifierResNet(nn.Module):
+    def __init__(self, num_classes=6):
+        super().__init__()
+        self.model = models.resnet50(weights='DEFAULT')
+        # Freeze all layers except the final fully connected layer
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # Unfreeze layer4 and fc layers
+        for param in self.model.layer4.parameters():
+            param.requires_grad = True
+
+            # Replace the final fully connected layer
+        self.model.fc = nn.Sequential(
+            nn.Dropout(0.3),
+            nn.Linear(self.model.fc.in_features, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+def predict(image_path):
+    image = Image.open(image_path).convert("RGB")
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    image_tensor = transform(image).unsqueeze(0) # (32, 3, 224, 224)
+
+    global trained_model
+
+    if trained_model is None:
+        trained_model = CarClassifierResNet()
+        trained_model.load_state_dict(torch.load("../streamlit-app/model/saved_model.pth"))
+        trained_model.eval()
+
+    with torch.no_grad():
+        output = trained_model(image_tensor) #[[12, 14, 9, 15, 8, 20]]
+        _, predicted_class = torch.max(output, 1) # 20(max_value), 5(index_value)
+        return class_names[predicted_class.item()]
